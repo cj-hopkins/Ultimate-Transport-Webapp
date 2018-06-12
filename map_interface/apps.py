b8d9bf20d30d66e5d66b397f1677b6b47b2fbc22
Conor Hopkins <conor.hopkins@ucdconnect.ie>
2018-06-12 01:04:32 +0100

    Hello world map app

---
+++ map_interface/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MapInterfaceConfig(AppConfig):
+    name = 'map_interface'
--- ultimate_transport/settings.py
+++ ultimate_transport/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'map_interface.apps.MapInterfaceConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
