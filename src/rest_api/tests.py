2eee548dc85a7e44d31f6f564ce3142ddb1568f7
Conor Hopkins <conor.hopkins@ucdconnect.ie>
2018-06-22 17:25:52 +0100

    added some basic api tests

--- src/rest_api/tests.py
+++ src/rest_api/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+import unittest
 
-# Create your tests here.
+class basicTestCase(TestCase):
+    client = Client()
+    
+    def test_index_page_load(self):
+        response = basicTestCase.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_stops_request(self):
+        response = basicTestCase.client.get('/api')
+        self.assertEqual(response.status_code, 200)
