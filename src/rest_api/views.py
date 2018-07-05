cb01e361186fedb319af2c6a3cee7197fa1cb2d1
Sophie Heseltine <sophieheseltine@Sophies-MacBook-Pro.local>
2018-07-05 13:02:09 +0100

    commit

--- src/rest_api/views.py
+++ src/rest_api/views.py
@@ -10,7 +10,7 @@ from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework.views import APIView
 from django.views.decorators.csrf import csrf_exempt
-from braces.views import CsrfExemptMixin
+#from braces.views import CsrfExemptMixin
 import random
 import json
 
