4c0307410fefaaa7ce4b58c3d3630ffb2eda3a7a
oleathlc <conor.lawlor@ucdconnect.ie>
2018-07-10 18:05:52 +0100

    Weather icon added

--- src/react_ui/src/components/Weather.js
+++ src/react_ui/src/components/Weather.js
@@ -1,4 +1,5 @@
 import React from "react";
+import WeatherIcon from "react-open-weather-icons";
 
 
 const API_KEY = "2abe029b7b8d40e80d1ed447f4522f0d";
@@ -10,19 +11,19 @@ class WeatherWidget extends React.Component{
     icon: undefined
   }
 
-  componentDidMount() {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=Dublin,ie&appid=${API_KEY}&units=metric`)
-      .then(res => res.json())
-      .then(
-        (data) => {
-          this.setState({
-            temperature: data.main.temp,
-            description: data.weather[0].description,
-            icon: data.weather[0].icon
-          });
-        },
-      )
-  }
+  // componentDidMount() {
+  //   fetch(`http://api.openweathermap.org/data/2.5/weather?q=Dublin,ie&appid=${API_KEY}&units=metric`)
+  //     .then(res => res.json())
+  //     .then(
+  //       (data) => {
+  //         this.setState({
+  //           temperature: data.main.temp,
+  //           description: data.weather[0].description,
+  //           icon: data.weather[0].icon
+  //         });
+  //       },
+  //     )
+  // }
 
 
   render() {
@@ -30,7 +31,7 @@ class WeatherWidget extends React.Component{
       <div>
       <p>Temperature: {this.state.temperature}</p>
       <p>Description: {this.state.description}</p> 
-              
+      <img src={`http://openweathermap.org/img/w/${this.state.icon}.png`}/>    
       </div>
       );
   }
