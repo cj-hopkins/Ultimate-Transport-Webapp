67627f485c8ab44c1432b887dab46aeba6f687ab
Orla Gartland <orla.gartland@ucdconnect.ie>
2018-07-16 15:44:53 +0100

    condensed time picker functionality into 1 file

--- src/react_ui/src/components/NowButton.js
+++ src/react_ui/src/components/NowButton.js
@@ -1,19 +1,28 @@
+import moment from 'moment';
 import React, { Component } from 'react';
 import { Button } from "react-bootstrap"
 
 
 class NowButton extends Component {
   state = {
-    date: new Date(),
+    date:moment()
+  }
+
+  handleClick = this.handleClick.bind(this);
+
+  handleClick(date) {
+    this.setState({
+      date: date
+    });
+    console.log('NOW BUTTON:'+this.state.date);
   }
  
- 
-  render() {
+render() {
     return (
       <div>
         <Button 
           value={this.state.date}
-        bsStyle="primary"> Leave now </Button>  
+        bsStyle="primary" onClick ={this.handleClick}> Leave now </Button>  
       </div>
     );
   }
--- src/react_ui/src/components/TimeButton.js
+++ src/react_ui/src/components/TimeButton.js
@@ -1,29 +1,80 @@
 import React, { Component } from "react"
-import TimeDropdown from './TimeDropdown'
 import { Button } from "react-bootstrap"
+import { Grid, Row, Col, Container } from 'react-bootstrap';
+import DatePicker from 'react-datepicker';
+import TimePicker from 'react-bootstrap-time-picker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
+
 
 class TimeButton extends Component {
-  constructor () {
-    super()
-    this.state = {
+  state = {
       isHidden: true
-    }
   }
   toggleHidden () {
     this.setState({
-      isHidden: !this.state.isHidden
+      isHidden: false
     })
   }
   render () {
     return (
       <div>
         <Button onClick={this.toggleHidden.bind(this)}  bsStyle='primary' >
-                 Change Time
+                 Change time and date
             </Button>
   
-        {!this.state.isHidden && <TimeDropdown />}
+        {!this.state.isHidden && <Grid><Row><Col><TimeDropdown /></Col><Col> <CalendarChooseDate/></Col></Row></Grid>}
       </div>
     )
+  }
+}
+
+class CalendarChooseDate extends Component {
+    state={  
+      startDate: moment()
+    };
+    handleChange = this.handleChange.bind(this);
+  
+ 
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
+    console.log('Chosen Date:'+this.state.startDate);
+  }
+ 
+  render() {
+    return <DatePicker
+        selected={this.state.startDate}   //when day is clicked
+        onChange={this.handleChange}   // when value has changed
+        minDate = {moment()}
+        highlightDates={[moment()]}         
+    />; 
+  }
+}
+
+
+class TimeDropdown extends Component {
+  constructor() {
+    super();
+
+    this.handleTimeChange = this.handleTimeChange.bind(this);
+
+    this.state = { time: 0 };
+  }
+
+  handleTimeChange(time) {
+    // seconds passed midnight (prints "3600" if "01:00" is picked) 
+    this.setState({ time });
+    console.log('Time chosen'+time)
+  }
+
+  render() {
+    return (
+      <div>
+        <TimePicker start="06:30" end="23:30" step={30}  onChange={this.handleTimeChange} value={this.state.time} />
+      </div>
+    );
   }
 }
 
