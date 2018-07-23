7112252b15350ac9052351352d3666be46130aff
Conor Hopkins <conor.hopkins@ucdconnect.ie>
2018-07-23 22:38:50 +0100

    basic async navigation in place

--- src/react_ui/src/components/CustomNavBar.jsx
+++ src/react_ui/src/components/CustomNavBar.jsx
@@ -6,28 +6,32 @@ class CustomNavbar extends Component {
     super(props);
   }
 
+
   render() {
     return (
-      <Navbar>
-        <Navbar.Header>
+      <Navbar style={{display:"flex", flexDirection:"row"}}>
+        {/* <Navbar.Header>
           <Navbar.Brand>
             <a href="#home">React-Bootstrap</a>
           </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
-          <NavItem eventKey={1} href="#">
-            Link
+        </Navbar.Header> */}
+        <Nav style={{display:"flex", flexDirection:"row"}}>
+          <NavItem eventKey={0} onSelect={this.props.swapUI}>
+            Bus Routes
           </NavItem>
-          <NavItem eventKey={2} href="#">
-            Link
+          <NavItem eventKey={1} onSelect={this.props.swapUI}>
+            Journey Planner
           </NavItem>
-          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+          <NavItem eventKey={2} onSelect={this.props.swapUI}>
+            Timetables
+          </NavItem>
+          {/* <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
             <MenuItem eventKey={3.1}>Action</MenuItem>
             <MenuItem eventKey={3.2}>Another action</MenuItem>
             <MenuItem eventKey={3.3}>Something else here</MenuItem>
             <MenuItem divider />
             <MenuItem eventKey={3.4}>Separated link</MenuItem>
-          </NavDropdown>
+          </NavDropdown> */}
         </Nav>
       </Navbar>
     );
--- src/react_ui/src/components/MainController.jsx
+++ src/react_ui/src/components/MainController.jsx
@@ -1,21 +1,57 @@
 import React, { Component } from "react";
 
-import { Button, ButtonGroup, Media } from "react-bootstrap"
-import {PageHeader} from 'react-bootstrap';
-import dublin_bus_icon from './dublin_bus_icon.png';
+import { Button, ButtonGroup, Media } from "react-bootstrap";
+import { PageHeader } from "react-bootstrap";
+import dublin_bus_icon from "./dublin_bus_icon.png";
 import WeatherWidget from "./Weather";
 
 import CustomNavbar from "./CustomNavBar";
 import ContentBlock from "./ContentBlock";
+import Example from "./examples/Example";
 
 class MainController extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      activatedUI: "ContentBlock"
+      activatedUI: 0,
+    //   UIkeys: {
+    //     0: "ContentBlock",
+    //     1: "JourneyPlanner",
+    //     2: "Timetables"
+    //   }
     };
   }
+
+  swapUI(key) {
+    //   console.log(key)
+    this.setState({
+      activatedUI: key
+    });
+  }
+  
+  renderSwitch = () => {
+      console.log("render switch")
+    switch (this.state.activatedUI) {
+        case 0:
+          return <ContentBlock key={0} 
+          onRouteUpdate={this.props.onRouteUpdate.bind(this)}
+          onSelectedJourneyUpdate={this.props.onSelectedJourneyUpdate.bind(this)}
+          />;
+        case 1:
+          return <Example key={1} />;
+        case 2:
+          return <ContentBlock key={2} />;
+        default:
+          return <div key={3} />;
+      }
+    // return <div>{chosenElement}</div>;
+  }
+
   render() {
+    // const key = this.state[this.state.activatedUI]
+    // console.log(key)
+    // const ChosenComponent = this.state.UIkeys[this.state.activatedUI];
+    // console.log(ChosenComponent);
     return (
       <div>
         <Media>
@@ -32,18 +68,37 @@ class MainController extends Component {
           </PageHeader>
           <WeatherWidget />
         </Media>
-
-        <CustomNavbar />
-        <ContentBlock
-          data={this.state.testState}
-          onRouteUpdate={this.props.onRouteUpdate.bind(this)}
-          onSelectedJourneyUpdate={this.props.onSelectedJourneyUpdate.bind(
-            this
-          )}
-        />
+        <CustomNavbar swapUI={this.swapUI.bind(this)} />
+        {/* Render chosen UI component - default to ContentBlock */}
+        {this.renderSwitch()}
       </div>
     );
   }
 }
+
+// class ChosenComponent extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
+//   render() {
+//     const { events } = this.props;
+//     const eventElement = {
+//       switch (this.props.choice) {
+//         case 0:
+//           return <ContentBlock key={shortid.generate()} 
+//           onRouteUpdate={this.props.onRouteUpdate.bind(this)}
+//           onSelectedJourneyUpdate={this.props.onSelectedJourneyUpdate.bind(this)}
+//           />;
+//         case 1:
+//           return <ContentBlock key={shortid.generate()} />;
+//         case 2:
+//           return <ContentBlock key={shortid.generate()} />;
+//         default:
+//           return <div key={shortid.generate()} />;
+//       }
+//     // return <div>{chosenElement}</div>;
+//   }
+// }
 
 export default MainController;
