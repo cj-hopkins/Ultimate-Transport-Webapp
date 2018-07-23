ef3cc24dc7b0669b81a3c586dcf8285ae828850b
Conor Hopkins <conor.hopkins@ucdconnect.ie>
2018-07-23 10:52:28 +0100

    Fixed a broken import statement

--- src/react_ui/src/components/ContentBlock.jsx
+++ src/react_ui/src/components/ContentBlock.jsx
@@ -2,7 +2,6 @@ import { Navbar, Nav, NavItem, MenuItem, NavDropdown, Grid, Row, Col, Container 
 import React, { Component } from "react"
 import RouteSelect from "./RouteSelect"
 import StopSelect from "./StopSelect"
-import NavigationTabs from './NavigationTabs';
 
 import { Button, ButtonGroup, Media } from "react-bootstrap"
 
@@ -226,28 +225,6 @@ class ContentBlock extends Component {
           </Media.Left><PageHeader className='fontForTitle'> Ultimate Transport Dublin</PageHeader><WeatherWidget/>
         </Media>
         {/* <NavigationTabs /> */}
-        <Navbar>
-  <Navbar.Header>
-    <Navbar.Brand>
-      <a href="#home">React-Bootstrap</a>
-    </Navbar.Brand>
-  </Navbar.Header>
-  <Nav>
-    <NavItem eventKey={1} href="#">
-      Link
-    </NavItem>
-    <NavItem eventKey={2} href="#">
-      Link
-    </NavItem>
-    <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-      <MenuItem eventKey={3.1}>Action</MenuItem>
-      <MenuItem eventKey={3.2}>Another action</MenuItem>
-      <MenuItem eventKey={3.3}>Something else here</MenuItem>
-      <MenuItem divider />
-      <MenuItem eventKey={3.4}>Separated link</MenuItem>
-    </NavDropdown>
-  </Nav>
-</Navbar>;
 
 	     <RouteSelect className="mb-3" onRouteUpdate={this.routeUpdate.bind(this)}
                       chosenRoute={this.state.chosenRoute}
