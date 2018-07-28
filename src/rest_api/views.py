0a08aa7c8b7075d743cfce39eee81857c2cba089
Orla Gartland <orla.gartland@ucdconnect.ie>
2018-07-28 22:08:29 +0100

    rain/no-rain for 5 days into furture now taken into account

--- src/rest_api/views.py
+++ src/rest_api/views.py
@@ -15,6 +15,9 @@ import random
 import json
 import re
 from rest_api.get_prediction import getPrediction, rf_model
+import pandas as pd
+import numpy as np 
+import datetime
 
 class getAllStops(generics.ListCreateAPIView):
     queryset = Stop.objects.all()
@@ -77,8 +80,10 @@ def getPredictionForJourney(request):
         print(result[0])
         return JsonResponse({'prediction':result[0]})
     else:
-        print(isDefaultTime)
-        return JsonResponse({'prediction':'NOT NOW!!!!!!!!!'})
+        isRaining = getRainNotNow(selectedTime,selectedDate )
+        result = getPrediction(numStops, isRaining, selectedTime)
+        print(result[0])
+        return JsonResponse({'prediction':str(result[0])+ 'NOT NOW!'})
 
 # class getStopsForRoute(CsrfExemptMixin, APIView):
 #     def post(self, request):
@@ -115,5 +120,56 @@ def getRainNow():
     isRaining = False if isRaining is None else True
     return isRaining
   
+def getRainNotNow(chosenTime, chosenDate):
+    regex = r'^.*[R|r]ain.*$'
+    future_weather = FiveDayWeather.objects.values()
+    relevant_weather= get_temp_and_rain(future_weather ,chosenTime, chosenDate )
+    isRaining = re.match(regex, relevant_weather['description'])
+    isRaining = False if isRaining is None else True
+    return isRaining
+  
+def get_temp_and_rain(response,seconds_past_midnight, epoch_time):
+    """
+    response: json from the getFiveDayWeather view in Django
+    seconds_past_midnight:Time chosen (current time or from Django)
+    epoch_time:Date chosen (from calendar)
+    """
+    def round_hour_nearest_3_hour(x):
+        """
+        Rounds to nearest 3 hour (also handles the edge case of 23:00)
+        """
+        if x==23:
+            ans= 0
+        else:
+            ans= int(3 * round(x/3))
+        return ans
+    
+    seconds_past_midnight_1970 =  pd.to_datetime(seconds_past_midnight,unit='s')  # seconds past midnight to dt 
+    nearest_hour = seconds_past_midnight_1970.hour                         # get hour
+    nearest_hour_multiple3 = round_hour_nearest_3_hour(nearest_hour)       # get nearest 3 hour interval
+    
+    epoch_dt_format = pd.to_datetime(epoch_time, unit='s')  # epoch time to dt object 
+    day_of_month_chosen = epoch_dt_format.day               # get day
+
+    for i in range(len(response)):    
+        entry = response[i]
+        time_for_entry =response[i]['timeofday']            #time in tring format
+        formatted_time= pd.to_datetime(time_for_entry, format='%Y-%m-%dT%H:00:00Z') # dt object
+        day_of_month = formatted_time.day
+        hour = formatted_time.hour             # hour of response(will be in multiple of 3)
+        day_of_week =formatted_time.dayofweek
+        
+        keys_for_dictionary = ['temp','rain','description']  # store relevant weather in dictionary
+        
+        res = {key: None for key in keys_for_dictionary}     # fill dictionary with Nones 
+        
+        if  day_of_month== day_of_month_chosen and hour==  nearest_hour_multiple3:
+            res['temp'] =  entry['temperature'] 
+            res['rain'] =  entry['rain']
+            res['description'] =  entry['description'] 
+            
+            return res  #if there's weather info return it else return dictionary of Nones
+    return res
+    
   
   
