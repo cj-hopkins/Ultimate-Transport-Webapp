b731dbfd38b6127e786c36ee4fe53a7b232e061a
oleathlc <conor.lawlor@ucdconnect.ie>
2018-08-17 19:04:52 +0100

    Renamed files didnt push

---
+++ src/react_ui/src/components/TimeTableStop.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Select from "react-select";
+
+class TimeTableStop extends Component {
+  handleStartSelect = start => {
+    if (start === null) {
+      this.props.onStopDeselect("start");
+    } else {
+      this.props.onStopUpdate(start.value, null);
+    }
+  };
+
+  handleToggle = event => {
+    this.props.onDirectionUpdate();
+  };
+
+  render() {
+    let stopsAsOptions = [];
+      this.props.stops.forEach(item =>
+        stopsAsOptions.push({
+          value: item.stop_id,
+          label: item.stop_id
+            .toString()
+            .concat(" ", item.location_text, " ", item.address)
+        })
+      );
+    return (
+      <div>
+        <Select
+          id="startSelect"
+          name="form-field-name"
+          options={stopsAsOptions}
+          value={this.props.startStop}
+          onChange={this.handleStartSelect}
+          placeholder={"Select stop"}
+        />
+      </div>
+    );
+  }
+}
+
+export default TimeTableStop;
